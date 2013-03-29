dd990f4ba20776292715f542dabdb5a1ec557e98
AdrianAlan <adrianalan.pol@gmail.com>
2013-03-29 17:15:17 +0100

    added popup WiFi Disabled

--- arsfest/src/dk/dtu/arsfest/MainActivity.java
+++ arsfest/src/dk/dtu/arsfest/MainActivity.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright 2013 Albert Fern�ndez de la Pe�a
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
 package dk.dtu.arsfest;
 
 import java.io.IOException;
@@ -28,8 +13,11 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.text.format.Time;
 import android.view.Menu;
@@ -37,9 +25,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	public static final String PREFS_NAME = "ArsFestPrefsFile";
 	private Button button;
 	private TextView days;
 	private TextView hours;
@@ -47,10 +37,9 @@ public class MainActivity extends Activity {
 	private TextView daysLabel;
 	private TextView hoursLabel;
 	private TextView minsLabel;
-	
+
 	private ArrayList<Location> locations;
 	private ArrayList<Event> events;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +67,7 @@ public class MainActivity extends Activity {
 					Constants.TYPEFONT_WELLFLEET));
 			minsLabel.setTypeface(Utils.getTypeface(this,
 					Constants.TYPEFONT_WELLFLEET));
-			
+
 			// Read from data.JSON
 			try {
 				readJson();
@@ -86,7 +75,7 @@ public class MainActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			// set countdown to arsfest start
 			showCountdown();
 
@@ -104,24 +93,13 @@ public class MainActivity extends Activity {
 			// CallRestServiceTask task = new CallRestServiceTask(this);
 			// task.execute();
 		} else {
-			setContentView(R.layout.wifi_popup);
-			TextView myError = (TextView) findViewById(R.id.textViewErrorCode);
-			myError.setText("There is no WiFi enabled right now. " +
-					"The application you want to use was aimed to use information " +
-					"provided by your WiFi card. Please enable your WiFi " +
-					"connection and try again later.");
+			/*
+			 * "There is no WiFi enabled right now. " +
+			 * "The application you want to use was aimed to use information " +
+			 * "provided by your WiFi card. Please enable your WiFi " +
+			 * "connection and try again later.");
+			 */
 		}
-	}
-
-	private boolean haveITurnedOnMyWiFi(Context applicationContext) {
-		ConnectivityManager myConnectivityManager = (ConnectivityManager) applicationContext
-				.getSystemService(CONNECTIVITY_SERVICE);
-		NetworkInfo myWiFi = myConnectivityManager
-				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		if (myWiFi.isAvailable()) {
-			return true;
-		}
-		return false;
 	}
 
 	private void showCountdown() {
@@ -144,15 +122,14 @@ public class MainActivity extends Activity {
 		new CustomCounter(millisOnFuture, 1000).start();
 	}
 
-	private void readJson() throws IOException{
-		
+	private void readJson() throws IOException {
+
 		JsonParser jsonParser = new JsonParser();
-		
+
 		this.locations = jsonParser.readLocations();
 		this.events = jsonParser.readEvents();
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -184,4 +161,54 @@ public class MainActivity extends Activity {
 
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		boolean popUpSettings = settings.getBoolean("popUpConnectivityDiscard",
+				false);
+		if (!haveITurnedOnMyWiFi(getApplicationContext()) && !popUpSettings) {
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+					this);
+			alertDialogBuilder.setTitle(R.string.wifi_title);
+			alertDialogBuilder
+					.setMessage(R.string.wifi_txt);
+			alertDialogBuilder.setPositiveButton(R.string.wifi_option1,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							startActivityForResult(new Intent(
+									android.provider.Settings.ACTION_SETTINGS),
+									id);
+							dialog.cancel();
+						}
+					}).setNegativeButton(R.string.wifi_option2,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							SharedPreferences settings = getSharedPreferences(
+									PREFS_NAME, 0);
+							SharedPreferences.Editor editor = settings.edit();
+							editor.putBoolean("popUpConnectivityDiscard", true);
+							editor.commit();
+
+							Toast.makeText(getApplicationContext(),
+									"WiFi card is off", Toast.LENGTH_SHORT)
+									.show();
+							dialog.cancel();
+						}
+					});
+			alertDialogBuilder.create().show();
+
+		}
+	}
+
+	private boolean haveITurnedOnMyWiFi(Context applicationContext) {
+		ConnectivityManager myConnectivityManager = (ConnectivityManager) applicationContext
+				.getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo myWiFi = myConnectivityManager
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (myWiFi.isAvailable()) {
+			return true;
+		}
+		return false;
+	}
 }
