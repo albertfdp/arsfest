1b27938a6817ddb5bfbfb590853ac891fc8661d1
AdrianAlan <adrianalan.pol@gmail.com>
2013-03-30 15:11:42 +0100

    event_activity added

---
+++ arsfest/src/dk/dtu/arsfest/event/EventActivity.java
@@ -0,0 +1,46 @@
+package dk.dtu.arsfest.event;
+
+import dk.dtu.arsfest.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class EventActivity extends Activity {
+
+	private TextView textViewEventName;
+	private TextView textViewTime;
+	private TextView textViewPlace;
+	private TextView textViewEventDescription;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event);
+
+		textViewEventName = (TextView) findViewById(R.id.textViewEventName);
+		textViewTime = (TextView) findViewById(R.id.textViewTime);
+		textViewPlace = (TextView) findViewById(R.id.textViewPlace);
+		textViewEventDescription = (TextView) findViewById(R.id.textViewEventDescription);
+
+		setEventName("The dinner");
+		setEventTime("3rd May, 21:00");
+		setEventPlace("The Canteen");
+		setEventDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+	}
+
+	private void setEventDescription(String string) {
+		textViewEventDescription.setText(string);
+	}
+
+	private void setEventPlace(String string) {
+		textViewPlace.setText(string);
+	}
+
+	private void setEventTime(String string) {
+		textViewTime.setText(string);
+	}
+
+	private void setEventName(String string) {
+		textViewEventName.setText(string);
+	}
+}
