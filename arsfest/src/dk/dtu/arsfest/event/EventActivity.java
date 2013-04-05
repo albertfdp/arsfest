b63f6b2d6a3cf9cc09093b336642d9bf1a724b23
AdrianAlan <adrianalan.pol@gmail.com>
2013-04-06 00:18:16 +0200

    fighting with layouts. ufff

--- arsfest/src/dk/dtu/arsfest/event/EventActivity.java
+++ arsfest/src/dk/dtu/arsfest/event/EventActivity.java
@@ -7,9 +7,12 @@ import dk.dtu.arsfest.utils.Utils;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class EventActivity extends Activity {
@@ -17,8 +20,9 @@ public class EventActivity extends Activity {
 	private TextView textViewEventName;
 	private TextView textViewTime;
 	private TextView textViewPlace;
-	private TextView textViewEventDescription;
+	private WebView textViewEventDescription;
 	private TextView headerTitle;
+	private LinearLayout backgroundImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,36 +30,46 @@ public class EventActivity extends Activity {
 		// hide title bar
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_event);
-		Typeface myNeoSans = Typeface.createFromAsset(getAssets(), "fonts/NeoSans.ttf");
-		
+		Typeface myNeoSans = Typeface.createFromAsset(getAssets(),
+				"fonts/NeoSans.ttf");
+
 		// update the font type of the header
 		headerTitle = (TextView) findViewById(R.id.actionBarTitle);
-		headerTitle.setTypeface(Utils.getTypeface(this, Constants.TYPEFONT_PROXIMANOVA));
+		headerTitle.setTypeface(Utils.getTypeface(this,
+				Constants.TYPEFONT_PROXIMANOVA));
 		headerTitle.setText(Constants.APP_NAME);
-		
+
 		// Read event info from intent
 		Intent intent = getIntent();
 		Event event = (Event) intent.getParcelableExtra("dk.dtu.arsfest.Event");
-		
+
 		Log.i("ARSFEST", event.getName() + " " + event.getStartTime());
 
+		Drawable background = getResources().getDrawable(R.drawable.background);
+		
+		backgroundImage = (LinearLayout) findViewById(R.id.eventBackground);
+		backgroundImage.setBackgroundDrawable(background);
 		textViewEventName = (TextView) findViewById(R.id.textViewEventName);
 		textViewEventName.setTypeface(myNeoSans);
 		textViewTime = (TextView) findViewById(R.id.textViewTime);
 		textViewTime.setTypeface(myNeoSans);
 		textViewPlace = (TextView) findViewById(R.id.textViewPlace);
 		textViewPlace.setTypeface(myNeoSans);
-		textViewEventDescription = (TextView) findViewById(R.id.textViewEventDescription);
-		textViewEventDescription.setTypeface(myNeoSans);
+		textViewEventDescription = (WebView) findViewById(R.id.textViewEventDescription);
+		textViewEventDescription.setVerticalScrollBarEnabled(true);
+		textViewEventDescription.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
 
 		setEventName(event.getName());
-		setEventTime("");
+		setEventTime("21:00");
 		setEventPlace("Somewhere");
-		setEventDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");	
+		setEventDescription("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 	}
 
 	private void setEventDescription(String string) {
-		textViewEventDescription.setText(string);
+		string = "<html><head><style type=\"text/css\">@font-face {font-family: 'myNeo'; src: url('file:///android_asset/fonts/NeoSans.ttf');}</style></head><body style=\"text-align:justify;font-family: 'myNeo', serif;background: #232323; color: ffffff;\">"
+				+ string + "</body></html>";
+		textViewEventDescription.loadDataWithBaseURL(null, string, "text/html",
+				"utf-8", "about:blank");
 	}
 
 	private void setEventPlace(String string) {
