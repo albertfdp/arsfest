950df1e206dae0dfb58b9593589c8e4417b32fdb
Albert Fernández <albertfdp@gmail.com>
2013-04-26 21:16:26 +0200

    fuck you adrian, solve your problems with your computer. :P

--- arsfest/src/dk/dtu/arsfest/utils/Constants.java
+++ arsfest/src/dk/dtu/arsfest/utils/Constants.java
@@ -75,5 +75,6 @@ public class Constants {
 	public static final String PREFERENCES_VIBRATION = "VibrationOnAlarm";
 
 	public static final String JSON_TAG_EVENT_REMARK = "remark";
+	public static final int MAX_EVENT_INFO = 450;
 
 }
