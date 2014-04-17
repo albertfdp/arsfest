ee1c03a20361b9373916cc86e174a59864afe1e1
Albert Fernández <albertfdp@gmail.com>
2014-04-17 16:04:54 +0200

    Update icons

---
+++ arsfest/src/dk/dtu/arsfest/ArsfestApp.java
@@ -0,0 +1,32 @@
+package dk.dtu.arsfest;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+public class ArsfestApp extends Application {
+	
+	public void onCreate() {
+		super.onCreate();
+		
+		// Create global configuration and initialize
+        // ImageLoader with this configuration
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+	    	.showImageOnLoading(R.drawable.ic_empty)
+	    	.showImageOnFail(R.drawable.ic_error)
+	    	.cacheInMemory(true)
+	    	.cacheOnDisc(true)
+	    	.build();
+        
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+			.defaultDisplayImageOptions(options)
+			.build();
+        
+        ImageLoader.getInstance().init(config);
+		
+		
+	}
+
+}
